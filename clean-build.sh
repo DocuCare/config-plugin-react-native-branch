b2a28703a8a9c45886140822c2dba39dd1026c1b
techgaun <coolsamar207@gmail.com>
2025-01-18 20:37:48 -0600

    feat: try deleting prepare and prepublishonly scripts

--- clean-build.sh
+++ clean-build.sh
@@ -8,6 +8,13 @@ git rm -rf .
 
 git checkout $MAIN_REF -- android ios app.plugin.js build expo-module.config.json package.json README.md
 
+node -e "
+  const pkg = require('./package.json');
+  delete pkg.scripts.prepare;
+  delete pkg.scripts.prepublishOnly;
+  require('fs').writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
+"
+
 git add android ios app.plugin.js build expo-module.config.json package.json README.md
 git commit -m "Build files for distribution"
 
