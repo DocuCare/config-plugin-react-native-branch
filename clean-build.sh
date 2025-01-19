927038af6e227de5891908e057033faba8325675
techgaun <coolsamar207@gmail.com>
2025-01-18 20:26:25 -0600

    feat: minor update

--- clean-build.sh
+++ clean-build.sh
@@ -5,9 +5,9 @@ git checkout --orphan build
 
 git rm -rf .
 
-git checkout $MAIN_REF -- android ios app.plugin.js build expo-module.config.json package.json
+git checkout $MAIN_REF -- android ios app.plugin.js build expo-module.config.json package.json README.md
 
-git add android ios app.plugin.js build expo-module.config.json package.json
+git add android ios app.plugin.js build expo-module.config.json package.json README.md
 git commit -m "Build files for distribution"
 
 git push origin build --force
