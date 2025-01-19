464b699327b638c66273d43ccb906369e0691eaa
techgaun <coolsamar207@gmail.com>
2025-01-18 20:22:13 -0600

    feat: refactor build script a little bit

--- clean-build.sh
+++ clean-build.sh
@@ -1,14 +1,11 @@
+git checkout main
+MAIN_REF=$(git rev-parse HEAD)
+
 git checkout --orphan build
 
 git rm -rf .
 
-mkdir -p build
-cp -r ../main/android .
-cp -r ../main/ios .
-cp ../main/app.plugin.js .
-cp -r ../main/build .
-cp ../main/expo-module.config.json .
-cp ../main/package.json .
+git checkout $MAIN_REF -- android ios app.plugin.js build expo-module.config.json package.json
 
 git add android ios app.plugin.js build expo-module.config.json package.json
 git commit -m "Build files for distribution"
